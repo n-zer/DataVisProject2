60c9a0fc255b7a1a7617119eb1d3e372f26cd1d3
Mike Teixeira <nai7804@ad.rit.edu>
2016-11-30 16:38:01 -0500

    Graph, outlier filtering

--- parser.js
+++ parser.js
@@ -1,8 +1,9 @@
 'use script'
-
+google.charts.load("current", {packages:['corechart']});
 document.querySelector("#files").addEventListener('change', function(e){
 	var modelFailures = {};
 	var modelDays = {};
+	var modelData = {};
 	var files = e.target.files;
 	var fileCount = 0;
 
@@ -69,7 +70,9 @@ document.querySelector("#files").addEventListener('change', function(e){
 		if(fileCount==files.length){
 			//remove diagnostic text
 			myNode.removeChild(myNode.firstChild);
-
+			var data = new google.visualization.DataTable();
+			data.addColumn('string','Models');
+			data.addColumn('number','Failure Rates');
 			//iterate through all models
 			for(var key in modelDays)
 			{
@@ -77,7 +80,48 @@ document.querySelector("#files").addEventListener('change', function(e){
 				var node = document.createElement("p");
 				node.innerHTML = key+ " Days: "+modelDays[key] +((modelFailures[key])? " Failures: "+modelFailures[key] : "");
 				myNode.appendChild(node);
+
+
+				//failures*100*365/driveDays
+				modelData[key] = modelFailures[key]*100*365/modelDays[key];
+				if(modelDays[key] > 45 && modelData[key]>0 && modelData[key] <100)
+					data.addRow([key,(modelFailures[key]/(modelDays[key]/365))*100]);
+				/*modelData[key] = {
+					failureRate: modelFailures[key]*100*365/modelDays[key],
+					failures: modelFailures[key],
+					driveDayrs:modelDays[key]
+				};*/
+				
 			}
+			var view = new google.visualization.DataView(data);
+			view.setColumns([0, 1,
+                  { calc: "stringify",
+                    sourceColumn: 1,
+                    type: "string",
+                    role: "annotation" }]);
+
+			var options = {
+				title: "Failure Rate of Popular Harddrive brands",
+				width: 600,
+				height: 400,
+				bar: {groupWidth: "95%"},
+				legend: { position: "none" },
+				series: {
+					0: {axis: 'brand'},
+					1: {axis: 'failureRate'}
+				},
+				axes: {
+					x: {
+						brand: {label: 'Brand'}
+					},
+					y: {
+						failurRate: {label: 'RoF'}
+					}
+				}
+			};
+			//var options = {};
+			var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+			chart.draw(view, options);
 		}
 	}
 });
